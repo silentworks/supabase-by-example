8bd63360f1e85917ae40951ad1884a0fe3198588
Andrew Smith <a.smith@silentworks.co.uk>
2024-09-03 09:37:23 +0000

    feat(remix): add phone otp auth

---
+++ remix/app/routes/_app.account.update-phone.tsx
@@ -0,0 +1,116 @@
+import { ActionFunctionArgs, LoaderFunctionArgs, json } from "@remix-run/node";
+import { Form, useActionData, useLoaderData } from "@remix-run/react";
+import { AuthApiError } from "@supabase/supabase-js";
+import { ZodError, z } from "zod";
+import Alert from "~/components/Alert";
+import InputErrorMessage from "~/components/InputErrorMessage";
+import { isUserAuthorized, isPasswordUpdateRequired } from "~/lib/session";
+import { createServerClient } from "~/lib/supabase";
+import { fault, formatError, getProfile, success } from "~/lib/utils";
+import { UpdatePhoneSchema } from "~/lib/validationSchema";
+
+type FormData = z.infer<typeof UpdatePhoneSchema>;
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { user, headers } = await isUserAuthorized(request)
+  await isPasswordUpdateRequired(request)
+
+  const { supabase } = createServerClient(request, request.headers);
+  const { profile } = await getProfile(supabase);
+
+	return json({ user, profile }, { headers });
+}
+
+export const action = async ({ 
+  request 
+}: ActionFunctionArgs) => {
+  const formData = await request.formData();
+  const phone = formData.get("phone") as string;
+  const phoneConfirm = formData.get("phoneConfirm") as string;
+  
+  try {
+    UpdatePhoneSchema.parse({ phone, phoneConfirm });
+  } catch (err) {
+    if (err instanceof ZodError) {
+      const errors = formatError(err) as FormData;
+      return json(fault({ data: { phone, phoneConfirm }, errors }));
+    }
+  }
+
+  const { supabase, headers } = createServerClient(request, new Headers());
+  
+  const { error } = await supabase.auth.updateUser({
+    phone
+  });
+
+  if (error) {
+    if (error instanceof AuthApiError && error.status === 400) {
+      return json(fault({ message: "Invalid credentials.", data: { phone: "", phoneConfirm: "" } }), { headers });
+    }
+    return json(fault({ message: error.message, data: { phone: "", phoneConfirm: "" } }), { headers });
+  }
+
+  return json(success({ 
+    message: "Your phone number was updated successfully.", 
+    data: { phone: "", phoneConfirm: "" } 
+  }), { headers });
+};
+
+export default function UpdatePhone() {
+  const actionData = useActionData<typeof action>();
+  const { user, profile } = useLoaderData<typeof loader>();
+  
+  return (
+    <div className="w-11/12 px-6 rounded-lg sm:w-8/12 md:w-6/12 2xl:w-3/12 sm:px-10">
+      {actionData?.message ? (
+        <Alert
+          className={`${actionData?.success ? "alert-info" : "alert-error"} mb-10`}
+        >
+          {actionData?.message}
+        </Alert>
+      ) : null}
+      <h2 className="font-semibold text-4xl mb-4">Update Phone</h2>
+      <p className="font-medium mb-4">
+        Hi {profile?.display_name ?? user?.email}, Enter your new phone below and confirm it
+      </p>
+      <Form method="post">
+        <div className="form-control">
+          <label htmlFor="phone" className="label">
+            Phone
+          </label>
+          <input
+            id="phone"
+            name="phone"
+            type="text"
+            defaultValue={actionData?.data?.phone}
+            className="input input-bordered"
+          />
+        </div>
+        {actionData?.errors?.phone ? (
+          <InputErrorMessage>{actionData.errors.phone}</InputErrorMessage>
+        ) : null}
+        <div className="form-control">
+          <label htmlFor="phoneConfirm" className="label">
+            Confirm Phone
+          </label>
+          <input
+            id="phoneConfirm"
+            name="phoneConfirm"
+            type="text"
+            defaultValue={actionData?.data?.phoneConfirm}
+            className="input input-bordered"
+          />
+        </div>
+        {actionData?.errors?.phoneConfirm ? (
+          <InputErrorMessage>{actionData.errors.phoneConfirm}</InputErrorMessage>
+        ) : null}
+        <div className="form-control mt-6">
+          <button className="btn btn-primary no-animation">
+            Update Phone
+          </button>
+        </div>
+      </Form>
+    </div>
+  );
+}
+  
--- remix/app/routes/auth.verify-token.tsx
+++ remix/app/routes/auth.verify-token.tsx
@@ -1,12 +1,13 @@
 import { ActionFunctionArgs, json, redirect } from "@remix-run/node";
-import { Form, Link, useActionData } from "@remix-run/react";
-import { AuthApiError, EmailOtpType } from "@supabase/supabase-js";
+import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
+import { AuthApiError, EmailOtpType, MobileOtpType } from "@supabase/supabase-js";
+import { useState } from "react";
 import { ZodError, z } from "zod";
 import Alert from "~/components/Alert";
 import InputErrorMessage from "~/components/InputErrorMessage";
 import { createServerClient } from "~/lib/supabase";
 import { fault, formatError } from "~/lib/utils";
-import { AuthUserWithTokenSchema } from "~/lib/validationSchema";
+import { AuthPhoneUserWithTokenSchema, AuthUserWithTokenSchema } from "~/lib/validationSchema";
 
 type FormData = z.infer<typeof AuthUserWithTokenSchema>;
 
@@ -14,35 +15,61 @@ export const action = async ({
   request 
 }: ActionFunctionArgs) => {
   const url = new URL(request.url);
-  const type = (url.searchParams.get("type") ?? "email") as EmailOtpType;
+  const type = (url.searchParams.get("type") ?? "email") as EmailOtpType | MobileOtpType;
   const nextUrl = url.searchParams.get("next") ?? "/";
 
   const formData = await request.formData();
   const email = formData.get("email") as string;
+  const phone = formData.get("phone") as string;
   const token = formData.get("token") as string;
   
-  try {
-    AuthUserWithTokenSchema.parse({ email, token });
-  } catch (err) {
-    if (err instanceof ZodError) {
-      const errors = formatError(err) as FormData;
-      return json(fault({ data: { email, token }, errors }));
-    }
-  }
-
   const { supabase, headers } = createServerClient(request, new Headers());
-  
-  const { error } = await supabase.auth.verifyOtp({
-    email,
-    token,
-    type,
-  });
 
-  if (error) {
-    if (error instanceof AuthApiError && error.status === 400) {
-      return json(fault({ message: "Invalid credentials.", data: { email, token: "" } }), { headers });
+  if (['sms', 'phone_change'].includes(type)) {
+    try {
+      AuthPhoneUserWithTokenSchema.parse({ phone, token });
+    } catch (err) {
+      if (err instanceof ZodError) {
+        const errors = formatError(err) as FormData;
+        return json(fault({ data: { phone, token, email: "" }, errors }));
+      }
     }
-    return json(fault({ message: error.message, data: { email, token: "" } }), { headers });
+    
+    const { error } = await supabase.auth.verifyOtp({
+      phone,
+      token,
+      type: type as MobileOtpType,
+    });
+
+    if (error) {
+      console.log(`Ex: ${token} ${error}`)
+      if (error instanceof AuthApiError && error.status === 400) {
+        return json(fault({ message: "Invalid credentials.", data: { phone, token: "", email: "" } }), { headers });
+      }
+      return json(fault({ message: error.message, data: { phone, token: "", email: "" } }), { headers });
+    }
+  } else {
+    try {
+      AuthUserWithTokenSchema.parse({ email, token });
+    } catch (err) {
+      if (err instanceof ZodError) {
+        const errors = formatError(err) as FormData;
+        return json(fault({ data: { email, token, phone: "" }, errors }));
+      }
+    }
+    
+    const { error } = await supabase.auth.verifyOtp({
+      email,
+      token,
+      type: type as EmailOtpType,
+    });
+
+    if (error) {
+      if (error instanceof AuthApiError && error.status === 400) {
+        return json(fault({ message: "Invalid credentials.", data: { email, token: "", phone: "" } }), { headers });
+      }
+      return json(fault({ message: error.message, data: { email, token: "", phone: "" } }), { headers });
+    }
   }
 
   return redirect(nextUrl, {
@@ -50,8 +77,62 @@ export const action = async ({
   });
 };
 
+interface PhoneFieldType {
+  phone?: string;
+  phoneError?: string; 
+}
+
+const PhoneField = ({ phone, phoneError }: PhoneFieldType ) => {
+  return <>
+    <div className="form-control">
+      <label htmlFor="phone" className="label">
+        Phone
+      </label>
+      <input
+        id="phone"
+        name="phone"
+        type="text"
+        defaultValue={phone}
+        className="input input-bordered"
+      />
+    </div>
+    {phoneError ? (
+      <InputErrorMessage>{phoneError}</InputErrorMessage>
+    ) : null}
+  </>
+}
+
+interface EmailFieldType {
+  email?: string;
+  emailError?: string; 
+}
+
+const EmailField = ({ email, emailError }: EmailFieldType ) => {
+  return <>
+    <div className="form-control">
+      <label htmlFor="email" className="label">
+        Email
+      </label>
+      <input
+        id="email"
+        name="email"
+        type="text"
+        defaultValue={email}
+        className="input input-bordered"
+      />
+    </div>
+    {emailError ? (
+      <InputErrorMessage>{emailError}</InputErrorMessage>
+    ) : null}
+  </>
+}
+
 export default function VerifyToken() {
   const actionData = useActionData<typeof action>();
+  const [searchParams, setSearchParams] = useSearchParams();
+  
+  const authType = searchParams.get('type') as string;
+  const [phoneLink, setPhoneLink] = useState(['sms', 'phone_change'].includes(authType));
   
   return (
     <div className="w-11/12 p-12 px-6 py-10 rounded-lg sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-3/12 sm:px-10 sm:py-6">
@@ -65,21 +146,11 @@ export default function VerifyToken() {
       <h2 className="font-semibold text-4xl mb-4">Sign in</h2>
       <p className="font-medium mb-4">Hi, Welcome back</p>
       <Form method="post">
-      <div className="form-control">
-          <label htmlFor="email" className="label">
-            Email
-          </label>
-          <input
-            id="email"
-            name="email"
-            type="text"
-            defaultValue={actionData?.data?.email}
-            className="input input-bordered"
-          />
-        </div>
-        {actionData?.errors?.email ? (
-          <InputErrorMessage>{actionData.errors.email}</InputErrorMessage>
-        ) : null}
+        {!phoneLink ? (
+          <EmailField email={actionData?.data?.email} emailError={actionData?.errors?.email} />
+        ) : (
+          <PhoneField phone={actionData?.data?.phone} phoneError={actionData?.errors?.phone} />
+        )}
         <div className="form-control">
           <label htmlFor="token" className="label">
             Token
